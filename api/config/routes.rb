ac0cce59b5d0aa2e80e0e60154efb8cc81bf548a
Helton <helton.dias.rodrigues@gmail.com>
2018-07-21 17:13:01 -0300

    configuracoes do resource para aceitar o json como resposta

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+ 
+
+  resources :category , :defaults => { :format => :json }
+  resources :product
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
