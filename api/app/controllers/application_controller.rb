e68b1ed587e74434db83438062576fa3aff0e7e0
Helton <helton.dias.rodrigues@gmail.com>
2018-07-22 00:30:00 -0300

    Correcao para conseguir cadastrar usuario utilizando o auth

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception, unless: -> { request.format.json? }
-  protect_from_forgery with: :exception, if: :isWebRequest?
+  protect_from_forgery with: :null_session
 
   def isWebRequest?
     request.subdomains[-1] != 'api'
